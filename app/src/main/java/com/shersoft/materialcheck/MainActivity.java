e4bff8dd5b2ac2d6acc28e2c2c2085c3b5dfc861
Hasif <lavahasif@gmail.com>
2019-12-02 21:33:37 +0530

    Initial commit

---
+++ app/src/main/java/com/shersoft/materialcheck/MainActivity.java
@@ -0,0 +1,14 @@
+package com.shersoft.materialcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
